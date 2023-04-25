9c32de1a614b16148a68a4de83b11616abca2781
AicheD <harryanthonyhz@hotmail.com>
2023-04-25 01:38:55 -0400

    Update main.py
    Cody look under the FUNCTIONAL comment and see the proposed parsing of data into maps

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 import http.client
+import json
 
 conn = http.client.HTTPSConnection("one.ufl.edu")
 
-conn.request("GET", "/apix/soc/schedule/?category=CWSP&term=2238&course-code=mac2311")
+conn.request("GET", "/apix/soc/schedule/?category=CWSP&term=2238")
 
 res = conn.getresponse()
 data = res.read()
 
+# Parse the JSON data
+parsed_data = json.loads(data)
+
+for i in range(len(parsed_data[0]["COURSES"])):
+    print(parsed_data[0]["COURSES"][i]["name"])
+    print([i])
+
+
+''' FUNCTIONAL
+# Create a dictionary of dictionaries, this collects genEd courses if there are any
+gen_ed_courses = {}
+for course in parsed_data[0]['COURSES']:
+    if 'genEd' in course:
+        for tag in course['genEd']:
+            if tag not in gen_ed_courses:
+                gen_ed_courses[tag] = {}
+            gen_ed_courses[tag][course['code']] = course
+
+# Print the dictionary of dictionaries
+print(gen_ed_courses)
+'''
+
+''' printing data --- testing
 print(data.decode("utf-8"))
+'''
